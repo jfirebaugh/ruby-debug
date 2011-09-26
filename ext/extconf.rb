0d3455436c5169442a8c779f338c38020b9801f6
John Firebaugh <john.firebaugh@gmail.com>
2011-09-25 17:07:54 -0700

    Remove version checks in extconf.rb

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,18 +1,5 @@
 require "mkmf"
 
-if RUBY_VERSION >= "1.9"
-  STDERR.print("Ruby version #{RUBY_VERSION} is too new\n")
-  exit(1)
-elsif RUBY_VERSION >= "1.8"
-  if RUBY_RELEASE_DATE < "2005-03-22"
-    STDERR.print("Ruby release date #{RUBY_RELEASE_DATE} is too old\n")
-    exit(1)
-  end
-else
-  STDERR.print("Ruby version is not compatible for this version.\n")
-  exit(1)
-end
-
 # Allow use customization of compile options. For example, the
 # following lines could be put in config_options to to turn off
 # optimization:
